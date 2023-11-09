19438f1201e9ce44e60675e693fc71560f74e18a
Emily Moreira Ferreira <emily.moreira.8@gmail.com>
2023-11-09 16:48:25 -0300

    Update planilhaQuemSomos.js

--- scripts/planilhaQuemSomos.js
+++ scripts/planilhaQuemSomos.js
@@ -4,6 +4,7 @@ function initGoogleSheetsApi() {
         discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     }).then(function () {
         loadProfessoresFromGoogleSheet();
+        loadBolsistasFromGoogleSheet() ;
     });
 }
 
@@ -34,6 +35,38 @@ function loadProfessoresFromGoogleSheet() {
                     </div>
                 `;
                 professoresContainer.appendChild(professorDiv);
+            });
+        }
+    });
+}
+
+function loadBolsistasFromGoogleSheet() {
+    const spreadsheetId = '1bnIVpHL_md8u_XXo-zA3ZJGbA2J_ijj0XtlJJjOPzvk'; 
+    const sheetName = 'pagina2'; // Altere para a folha correta onde estão os dados dos bolsistas
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: sheetName
+    }).then(function (response) {
+        const data = response.result.values;
+
+        if (data.length > 0) {
+            const bolsistasContainer = document.getElementById('bolsistas-container');
+            data.forEach(function (row) {
+                const nome = row[0];
+                const area = row[1];
+                const anoPeriodo = row[2];
+                const foto = row[3];
+                const bolsistaDiv = document.createElement('div');
+                bolsistaDiv.className = 'col-md-4 bolsista';
+                bolsistaDiv.innerHTML = `
+                    <div class="bolsista">
+                        <img src="${foto}" alt="${nome}" class="img-fluid">
+                        <h3>${nome}</h3>
+                        <p>Curso: ${area}</p>
+                        <p>Ano/Período: ${anoPeriodo}</p>
+                    </div>
+                `;
+                bolsistasContainer.appendChild(bolsistaDiv);
             });
         }
     });
